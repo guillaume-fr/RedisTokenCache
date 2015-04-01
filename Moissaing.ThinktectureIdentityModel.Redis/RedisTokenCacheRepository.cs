4aa3892c472aaa3db197383f3bcf1257d33a164a
guillaume-fr <guillaume.moissaing@gmail.com>
2015-04-01 15:54:28 +0200

    Initial commit

---
+++ Moissaing.ThinktectureIdentityModel.Redis/RedisTokenCacheRepository.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using Thinktecture.IdentityModel.Web;
+
+namespace Moissaing.ThinktectureIdentityModel.Redis
+{
+  /// <summary>
+  /// Implementation of token cache repository using redis storage
+  /// </summary>
+  public sealed class RedisTokenCacheRepository : ITokenCacheRepository, IDisposable
+  {
+    /// <summary>
+    /// Intializes an instance of RedisTokenCacheRepository
+    /// </summary>
+    /// <param name="connectionString">Redis connection string, see StackExchange.Redis documentation</param>
+    /// <param name="applicationName">Prefix used in cache key to avoid collisions between applications</param>
+    public RedisTokenCacheRepository(string connectionString, string applicationName)
+    {
+      _connection = ConnectionMultiplexer.Connect(connectionString);
+      _applicationName = applicationName;
+    }
+
+    private readonly string _applicationName;
+    private ConnectionMultiplexer _connection;
+
+    /// <summary>
+    /// Adds or updates the token in the cache
+    /// </summary>
+    public void AddOrUpdate(TokenCacheItem item)
+    {
+      if (item == null)
+        throw new ArgumentNullException("item");
+      var db = _connection.GetDatabase();
+      TimeSpan expiry = item.Expires.ToUniversalTime() - DateTime.UtcNow;
+      db.StringSet(_applicationName + item.Key, item.Token, expiry);
+    }
+
+    /// <summary>
+    /// Gets a token from the cache
+    /// </summary>
+    public TokenCacheItem Get(string key)
+    {
+      var db = _connection.GetDatabase();
+      var cachedValue = db.StringGet(_applicationName + key);
+      if (cachedValue.IsNull)
+        return null;
+      else
+        return new TokenCacheItem() { Key = key, Token = cachedValue };
+    }
+
+    /// <summary>
+    /// Removes a token from the cache
+    /// </summary>
+    public void Remove(string key)
+    {
+      var db = _connection.GetDatabase();
+      db.KeyDelete(_applicationName + key);
+    }
+
+    /// <summary>
+    /// Release all resources associated with this object
+    /// </summary>
+    public void Dispose()
+    {
+      _connection.Dispose();
+    }
+  }
+}
---
+++ RedisTokenCache.Tests/TokenRepositoryTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moissaing.ThinktectureIdentityModel.Redis;
+using Thinktecture.IdentityModel.Web;
+
+namespace RedisTokenCache.Tests
+{
+  [TestClass]
+  public class TokenRepositoryTests
+  {
+    private Process serverProcess;
+    private const string connectionString = "localhost,connectRetry=3,connectTimeout=500";
+
+    [TestInitialize]
+    public void Initialize()
+    {
+      serverProcess = Process.Start(@"..\..\..\packages\Redis-32.2.6.12.1\tools\redis-server.exe");
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+      try
+      {
+        serverProcess.Kill();
+      }
+      catch { }
+      serverProcess.Dispose();
+    }
+
+    [TestMethod]
+    public void TestAddOrUpdateThenGet()
+    {
+      var repository = new RedisTokenCacheRepository(connectionString, "UnitTests");
+
+      repository.AddOrUpdate(new TokenCacheItem() { Key = "42", Expires = DateTime.UtcNow.AddMinutes(1), Token = new byte[] { 0, 1, 2 } });
+      var item = repository.Get("42");
+
+      Assert.IsNotNull(item);
+      CollectionAssert.AreEqual(new byte[] { 0, 1, 2 }, item.Token);
+      Assert.AreEqual("42", item.Key);
+    }
+    
+    [TestMethod]
+    public void TestUpdateExisting()
+    {
+      var repository = new RedisTokenCacheRepository(connectionString, "UnitTests");
+
+      repository.AddOrUpdate(new TokenCacheItem() { Key = "42", Expires = DateTime.UtcNow.AddMinutes(1), Token = new byte[] { 0, 1, 2 } });
+      var item = repository.Get("42");
+      repository.AddOrUpdate(new TokenCacheItem() { Key = "42", Expires = DateTime.UtcNow.AddMinutes(1), Token = new byte[] { 3, 4, 5 } });
+      var item2 = repository.Get("42");
+
+      Assert.IsNotNull(item);
+      CollectionAssert.AreEqual(new byte[] { 0, 1, 2 }, item.Token);
+      Assert.AreEqual("42", item.Key);
+      Assert.IsNotNull(item2);
+      CollectionAssert.AreEqual(new byte[] { 3, 4, 5 }, item2.Token);
+      Assert.AreEqual("42", item2.Key);
+    }
+
+    [TestMethod]
+    public void TestDeleteExisting()
+    {
+      var repository = new RedisTokenCacheRepository(connectionString, "UnitTests");
+      repository.AddOrUpdate(new TokenCacheItem() { Key = "42", Expires = DateTime.UtcNow.AddMinutes(1), Token = new byte[] { 0, 1, 2 } });
+      Assert.IsNotNull(repository.Get("42"));
+
+      repository.Remove("42");
+
+      Assert.IsNull(repository.Get("42"));
+    }
+    
+    [TestMethod]
+    public void TestExpiration()
+    {
+      var repository = new RedisTokenCacheRepository(connectionString, "UnitTests");
+      repository.AddOrUpdate(new TokenCacheItem() { Key = "42", Expires = DateTime.UtcNow.AddMilliseconds(200), Token = new byte[] { 0, 1, 2 } });
+      Assert.IsNotNull(repository.Get("42"));
+
+      Thread.Sleep(400);
+
+      Assert.IsNull(repository.Get("42"));
+    }
+    
+    [TestMethod]
+    public void TestGetMissing()
+    {
+      var repository = new RedisTokenCacheRepository(connectionString, "UnitTests");
+      
+      Assert.IsNull(repository.Get("doesnt.exists"));
+    }
+
+    [TestMethod]
+    public void TestDeleteMissing()
+    {
+      var repository = new RedisTokenCacheRepository(connectionString, "UnitTests");
+
+      repository.Remove("doesnt.exists");
+
+      Assert.IsNull(repository.Get("doesnt.exists"));
+    }
+  }
+}
